2efaf94df297eada262e73dd7b870406fbbe8e05
MK-320 <321123809@qq.com>
2022-05-08 13:35:01 +0800

    1

---
+++ test.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: Drgeek
+ * @Date: 2022-05-08 13:34:33
+ * @LastEditors: Drgeek
+ * @LastEditTime: 2022-05-08 13:34:42
+ * @FilePath: \MIMall\test.js
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by drgeek/noCompany, All Rights Reserved. 
+ */
+var a=1;
